b059a29d719b36525f67954fc5010c2ebf8b87be
diresh-shrestha <direshshrestha95@gmail.com>
2020-10-29 21:00:39 -0500

    add notes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
 function App() {
+  const note = notes.map((obj) => {
+    return <Note title={obj.title} content={obj.content} />;
+  });
   return (
     <div>
       <Header />
-      <Note />
+      {note}
       <Footer />
     </div>
   );
